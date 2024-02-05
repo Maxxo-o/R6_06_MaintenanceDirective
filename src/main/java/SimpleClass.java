3a9938edc20dcaa86a9e87c17c0d9ef09ac79da7
Maxime Segot <max.segot@gmail.com>
2024-02-05 15:27:51 +0100

    premier refactoé

---
+++ src/main/java/SimpleClass.java
@@ -0,0 +1,8 @@
+public class SimpleClass {
+    public static void main(String[] args) {
+        new SimpleClass().add(1, 2);
+    }
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
